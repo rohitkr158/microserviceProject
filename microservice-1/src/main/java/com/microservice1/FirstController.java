6fd7b72dc9e802bc8dc8127c3369239beb9a4895
rohitkr158 <rohitkr158@gmail.com>
2025-07-30 12:45:52 +0530

    Third commit

---
+++ microservice-1/src/main/java/com/microservice1/FirstController.java
@@ -0,0 +1,12 @@
+package com.microservice1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+	@GetMapping("/message")
+	public String getMessage() {
+		return "From MS 1";
+	}
+}
